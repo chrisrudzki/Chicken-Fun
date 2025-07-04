88789951c6fdc31895dc1fb07c79069c32ecc645
chrisrudzki <chrisrudzki7@gmail.com>
2025-07-04 15:39:37 -0400

    edit wall file

--- wall.gd
+++ wall.gd
@@ -5,50 +5,49 @@ extends CharacterBody2D
 @onready var timer = $Timer
 @onready var stop_timer = $stop_timer
 
-#var dir : float
 var spawn_pos : Vector2
+
 var spawn_rot : float
+
 var x
+
 var loc_mouse_pos : Vector2
+
 var wall_num : int
+
+var dmg = 1000
 
 var is_stopped = false
 
-
-func hitwall():
-	#velocity = Vector2.ZERO
-	velocity = velocity / 7
-	stop_timer.start(.05)
-	print("hitanother wall")
-	
-	
 func wall():
 	pass
 
+# when the wall hits an object
+func hitwall():
+	velocity = velocity / 7
+	stop_timer.start(.05)
+
 func _ready():
+	
 	print(x)
+	
 	position = spawn_pos
+	
 	rotation = spawn_rot
 	
 	timer.start(25)
 	
+# 
 func _physics_process(delta):
-	#print(x)
 	
+	velocity = x * speed
 	
-	
-	if x != null and is_stopped == false:
-		velocity = x * speed
-	else:
-		velocity = Vector2.ZERO
-	#velocity = Vector2(0,-speed).rotated(dir)
-		
-	#print("position: ", position)
-	#print("dest mouse position: ", loc_mouse_pos)	
-		
+	# stop if wall has reached the mouse position
 	if Vector2(2,2) > position - loc_mouse_pos and Vector2(-2,-2) < position - loc_mouse_pos:
 		velocity = Vector2.ZERO
 		
+	if is_stopped:
+		velocity = Vector2.ZERO
 	
 	move_and_slide()
 
@@ -61,13 +60,13 @@ func _on_hitbox_body_entered(body: Node2D) -> void:
 	
 	if body.has_method("wall") and body.wall_num != wall_num:
 		body.hitwall()
-
-
-func _on_hitbox_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
-
+		
+	if body.has_method("bullet"):
+		body.velocity = Vector2.ZERO
+		
+	if body.has_method("boid"):
+		is_stopped = true
+		
 
 func _on_stop_timer_timeout() -> void:
-	print("got here")
-	#velocity = Vector2.ZERO
 	is_stopped = true
